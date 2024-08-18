e2dc997327922df8f0b10dc4287f1e5517532ecf
Ali Hassan <layanthebot@gmail.com>
2024-08-18 22:52:08 +0500

    add inventory and products page

---
+++ client/src/app/products/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useGetProductsQuery } from "@/state/api";
+import { PlusCircleIcon, SearchIcon } from "lucide-react";
+import { useState } from "react";
+import Header from "@/app/(components)/Header";
+
+const Products = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const {
+    data: products,
+    isError,
+    isLoading,
+  } = useGetProductsQuery(searchTerm);
+
+  if (isLoading) return <div className="py-4">Loading...</div>;
+
+  if (isError || !products) {
+    return (
+      <div className="text-center text-red-500 py-4">
+        Failed to fetch products
+      </div>
+    );
+  }
+
+  return (
+    <div className="mx-auto pb-5 w-full ">
+      {/* search bar */}
+      <div className="mb-6">
+        <div className="flex items-center border-2 border-gray-200 rounded">
+          <SearchIcon className="w-5 h-5 text-gray-500 m-2" />
+          <input
+            className="w-full py-2 px-4 rounded bg-white"
+            placeholder="Search products..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      </div>
+
+      {/* header bar */}
+      <div className="flex justify-between items-center mb-6">
+        <Header name="Products" />
+        <button
+          className="flex items-center bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded"
+          onClick={() => setIsModalOpen(true)}
+        >
+          <PlusCircleIcon className="w-5 h-5 mr-2 text-gray-200" />
+          Create Product
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
